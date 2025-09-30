41457c9981488eaf6f8353861ddf8c9a3d9e0368
Bhavanti <routbhavanti06@gmail.com>
2025-09-30 19:14:02 +0530

    Assignment-1

---
+++ Assignment-1.c
@@ -0,0 +1,7 @@
+//#ASSIGNMENT-1
+#include <stdio.h>
+void main(){
+    int c;
+    c=5+2;
+    printf("The sum of 5 and 2 is =%d",c);
+}
---
+++ Assignment-2.c
@@ -0,0 +1,9 @@
+//#ASSIGNMENT-2 
+#include <stdio.h>
+void main(){
+    int a,b,c;
+    printf("Enter any two numbers");
+    scanf("%d,%d",&a,&b);
+    c=a+b;
+    printf("The sum of the two numbers %d + %d = %d",a,b,c);
+}
