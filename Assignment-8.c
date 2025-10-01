6b22fb5691f9e3f5a08dcc87479efb07bc357b52
Bhavanti <routbhavanti06@gmail.com>
2025-10-01 11:43:54 +0530

    Ass-8

---
+++ Assignment-8.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+void main(){
+    int opt;
+    printf("Enter any number between 1-7:");
+    scanf("%d",&opt);
+    switch (opt)
+    {
+        case 1:
+        printf("Monday");
+        break;
+        case 2:
+        printf("Tuesday");
+        break;
+        case 3:
+        printf("Wednesday");
+        break;
+        case 4:
+        printf("Thursday");
+        break;
+        case 5:
+        printf("Friday");
+        break;
+        case 6:
+        printf("Saturday");
+        break;
+        case 7:
+        printf("Sunday");
+        break;
+    
+    default:
+    printf("Enter a valid number");
+        break;
+    }
+}
