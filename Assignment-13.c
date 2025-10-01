9b2e040df04ef1ff34676d69603518f71e5fc887
Bhavanti <routbhavanti06@gmail.com>
2025-10-02 00:58:31 +0530

    New file added

---
+++ Assignment-13.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void main(){
+    int a[3][2]={
+        {1,2},
+        {3,6},
+        {9,8}
+    };
+    printf("The elements of the array are: \n");
+    for(int i=0; i<3; i++){
+        for(int j=0; j<2; j++){
+            printf("%d",a[i][j]);
+            printf("\t");
+        }
+         printf("\n");
+    }
+    
+}
---
+++ Assignment-14.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+void main(){
+    int a[2][3][4]={
+        {
+            {1,2,3,4},
+            {6,8,9,7},
+            {5,10,11,12}
+        },
+        {
+            {21,50,30,40},
+            {20,60,70,80},
+            {14,16,18,22}
+        }
+    };
+    printf("The elements of the array are: \n");
+    for(int i=0; i<2; i++){
+        for(int j=0; j<3; j++){
+            for(int k=0; k<4; k++){
+                printf("%d",a[i][j][k]);
+                printf("\t");
+
+            }
+             printf("\n");
+        }
+       
+    }
+}
